5c2fbbda44cb72dc10ae974717f46186779200d8
qbhy <96qbhy@gmail.com>
2022-01-20 18:35:38 +0800

    doc: 添加部分方法注释

--- between.go
+++ between.go
@@ -2,6 +2,7 @@ package querybuilder
 
 import "github.com/goal-web/contracts"
 
+// WhereBetween args 参数可以是整数、浮点数、字符串、interface{} 等类型的数组，或者用` and `隔开的字符串，或者在源码中了解更多 https://github.com/goal-web/querybuilder/blob/78bcc832604bfcdb68579e3dd1441796a16994cf/builder.go#L74
 func (this *Builder) WhereBetween(field string, args interface{}, whereType ...contracts.WhereJoinType) contracts.QueryBuilder {
 	if len(whereType) > 0 {
 		return this.Where(field, "between", args, whereType[0])
